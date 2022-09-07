c4c45965f9e04b61c5c925765682b1df41084abe
Grigorii Melnikov <melnikovg97@gmail.com>
2022-09-07 14:02:18 +0300

    Basic Files added

---
+++ Dockerfiles/AMD/Dockerfile[Py3.8.13-Hityatzvut-AMD].sh
@@ -0,0 +1,148 @@
+#
+# NOTE: THIS DOCKERFILE IS GENERATED VIA "apply-templates.sh"
+#
+# PLEASE DO NOT EDIT IT DIRECTLY.
+#
+
+FROM buildpack-deps:bullseye
+
+# ==================================================================================
+# PYTHON INSTALLATION
+# Skip to the next block
+# ==================================================================================
+
+# ensure local python is preferred over distribution python
+ENV PATH /usr/local/bin:$PATH
+
+# http://bugs.python.org/issue19846
+# > At the moment, setting "LANG=C" on a Linux system *fundamentally breaks Python 3*, and that's not OK.
+ENV LANG C.UTF-8
+
+# runtime dependencies
+RUN set -eux; \
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		libbluetooth-dev \
+		tk-dev \
+		uuid-dev \
+	; \
+	rm -rf /var/lib/apt/lists/*
+
+ENV GPG_KEY E3FF2839C048B25C084DEBE9B26995E310250568
+ENV PYTHON_VERSION 3.8.13
+
+RUN set -eux; \
+	\
+	wget -O python.tar.xz "https://www.python.org/ftp/python/${PYTHON_VERSION%%[a-z]*}/Python-$PYTHON_VERSION.tar.xz"; \
+	wget -O python.tar.xz.asc "https://www.python.org/ftp/python/${PYTHON_VERSION%%[a-z]*}/Python-$PYTHON_VERSION.tar.xz.asc"; \
+	GNUPGHOME="$(mktemp -d)"; export GNUPGHOME; \
+	gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$GPG_KEY"; \
+	gpg --batch --verify python.tar.xz.asc python.tar.xz; \
+	command -v gpgconf > /dev/null && gpgconf --kill all || :; \
+	rm -rf "$GNUPGHOME" python.tar.xz.asc; \
+	mkdir -p /usr/src/python; \
+	tar --extract --directory /usr/src/python --strip-components=1 --file python.tar.xz; \
+	rm python.tar.xz; \
+	\
+	cd /usr/src/python; \
+	gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)"; \
+	./configure \
+		--build="$gnuArch" \
+		--enable-loadable-sqlite-extensions \
+		--enable-optimizations \
+		--enable-option-checking=fatal \
+		--enable-shared \
+		--with-system-expat \
+		--without-ensurepip \
+	; \
+	nproc="$(nproc)"; \
+	make -j "$nproc" \
+	; \
+	make install; \
+	\
+# enable GDB to load debugging data: https://github.com/docker-library/python/pull/701
+	bin="$(readlink -ve /usr/local/bin/python3)"; \
+	dir="$(dirname "$bin")"; \
+	mkdir -p "/usr/share/gdb/auto-load/$dir"; \
+	cp -vL Tools/gdb/libpython.py "/usr/share/gdb/auto-load/$bin-gdb.py"; \
+	\
+	cd /; \
+	rm -rf /usr/src/python; \
+	\
+	find /usr/local -depth \
+		\( \
+			\( -type d -a \( -name test -o -name tests -o -name idle_test \) \) \
+			-o \( -type f -a \( -name '*.pyc' -o -name '*.pyo' -o -name 'libpython*.a' \) \) \
+			-o \( -type f -a -name 'wininst-*.exe' \) \
+		\) -exec rm -rf '{}' + \
+	; \
+	\
+	ldconfig; \
+	\
+	python3 --version
+
+# make some useful symlinks that are expected to exist ("/usr/local/bin/python" and friends)
+RUN set -eux; \
+	for src in idle3 pydoc3 python3 python3-config; do \
+		dst="$(echo "$src" | tr -d 3)"; \
+		[ -s "/usr/local/bin/$src" ]; \
+		[ ! -e "/usr/local/bin/$dst" ]; \
+		ln -svT "$src" "/usr/local/bin/$dst"; \
+	done
+
+# if this is called "PIP_VERSION", pip explodes with "ValueError: invalid truth value '<VERSION>'"
+ENV PYTHON_PIP_VERSION 21.3.1
+# https://github.com/docker-library/python/issues/365
+ENV PYTHON_SETUPTOOLS_VERSION 60.5.0
+# https://github.com/pypa/get-pip
+ENV PYTHON_GET_PIP_URL https://raw.githubusercontent.com/pypa/get-pip/3cb8888cc2869620f57d5d2da64da38f516078c7/public/get-pip.py
+#ENV PYTHON_GET_PIP_SHA256 deaf32dcd9ab821e359cd8330786bcd077604b5c5730c0b096eda46f95c24a2d
+
+
+RUN set -eux; \
+	\
+	# wget -O get-pip.py "$PYTHON_GET_PIP_URL"; \
+	# echo "$PYTHON_GET_PIP_SHA256 *get-pip.py" | sha256sum -c -; \
+	curl -0 "$PYTHON_GET_PIP_URL" > get-pip.py; \
+	\
+	export PYTHONDONTWRITEBYTECODE=1; \
+	\
+	python get-pip.py \
+		--disable-pip-version-check \
+		--no-cache-dir \
+		--no-compile \
+		"pip==$PYTHON_PIP_VERSION" \
+		"setuptools==$PYTHON_SETUPTOOLS_VERSION" \
+	; \
+	pip config set \
+		global.disable-pip-version-check true; \
+
+	rm -f get-pip.py; \
+	\
+	pip --version
+
+# ==================================================================================
+# PYTHON SETUP
+# Loading packages
+# ==================================================================================
+# set the working directory in the container
+WORKDIR /mnt
+
+# copy the dependencies file to the working directory
+COPY Include/Hityatzvut-Env include/
+
+# install dependencies/environment
+RUN pip config set global.disable-pip-version-check true; \
+	# pip install --no-cache-dir --upgrade pip; \
+  pip install --no-cache-dir -r include/requirements.txt
+
+# ==================================================================================
+# SET BOOT UP SCRIPT
+# Bash script which will run on container start
+# ==================================================================================
+
+COPY Entrypoint/entrypoint.sh /usr/entrypoint.sh
+
+RUN chmod +x /usr/entrypoint.sh
+
+CMD ["/bin/bash"]
